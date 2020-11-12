e39134a6e5f653107e41b3b8a2336e61fe757372
qsk17322028282 <1527931431@qq.com>
2020-11-12 11:00:22 +0800

    删除单独下载webpack对项目的影响

---
+++ config/webpack.config.js
@@ -0,0 +1,19 @@
+// process.env.NODE_ENV = 'development' // 
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const {resolve}= require('path')
+// module.exports = {
+//   entry:'../src/index.js',
+//   output: {
+//     filename: '[name].js',
+//     path:resolve(__dirname , 'build') 
+//   },
+//   module:{
+//     rules:[]
+//   },
+//   plugins:[
+//     new HtmlWebpackPlugin({
+//       template:'../public/index.html'
+//     })
+//   ],
+//   mode:'development'
+// }
